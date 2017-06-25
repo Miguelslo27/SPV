65d9757641def4d07891f6f45bc4560e7d5195da
Miguel Angel Sosa Menchaca <miguel.sosa@globant.com>
2017-06-25 13:00:18 -0300

    HotFix - Solucion a error en las rutas de los archivos al envio del email

--- lib/functions.php
+++ lib/functions.php
@@ -136,7 +136,7 @@ function savePoliza($data, $pdfRoute) {
 }
 
 function sendEmail($dest, $data, $pdfRoute) {
-  $serverhost = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'];
+  $serverhost = '://'.$_SERVER['HTTP_HOST'];
   $pdf = $serverhost.$pdfRoute;
   $logo = $serverhost.'/imagenes/logos/logo.png';
   $categoria = $data['categoria'];
